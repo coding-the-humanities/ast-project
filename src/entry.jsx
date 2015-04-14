dda7c51da4ab3e7664e93f0efa18226e67f64a30
Zaturrby <rjkorteschiel@gmail.com>
2015-04-14 22:54:42 +0200

    code from lecture

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
 
-class Test extends React.Component {
+class GameBoardCell extends React.Component {
+	handleClick(cellState){
+		let message = cellState ? "hit" : "miss";
+		console.log(message);
+	}
+
   render(){
     return (
-      <h1>HELLO WORLD</h1>
+      <button onClick={this.handleClick.bind(this, this.props.state) }>GameBoardCell</button>
     )
   }
 }
 
-React.render(<Test />, document.body);
+class SwitchBoardCell extends React.Component {
+  render(){
+    let state = this.props.state;
+    let index = this.props.index;
+    return (
+      <button onClick={ this.props.toggleState.bind(this, index) }>{ this.props.state ? 'on' : 'off' }</button>
+    )
+	}
+}
+
+class Game extends React.Component {
+  constructor(props){
+		super(props);
+		this.state = {
+			board: [0,1,0,1,0,1,0,1]
+		}
+	}
+
+	toggleState(index){
+		console.log(index);
+		let board = this.state.board;
+		board[index] = !board[index];
+		this.setState({board})
+	}
+
+  render(){  	
+  	let board = this.state.board;
+  	
+  	return (
+  		<section className="game">
+  		  <section className="gameboard">
+  		    { board.map((value, index) => <GameBoardCell key={ index } index={ index } state={ value }/>) }
+  		  </section>
+  		  <section className="switchboard">
+  		   	{ board.map((value, index) => <SwitchBoardCell toggleState={ this.toggleState.bind(this) } key={ index } index={ index } state={ value }/>) }
+  		  </section>        
+   		</section>
+  	)
+  }
+}
+
+React.render(<Game />, document.body);
